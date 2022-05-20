fb61c2e06146dfda30edf4d3bf0b5fc18a656662
Dmitry <d.spitsyn@internet.ru>
2022-05-20 14:45:44 +0300

    задача 3

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,14 @@ public class Main {
         } else if (clientOS == 1 && clientDeviceYear < 2015) {System.out.println("Установить  облегченную приложения для Android по ссылке");}
         if (clientOS == 0 && clientDeviceYear < 2015) {System.out.println("Установить облегченную версию приложения для iOS по ссылке");}
 
+        // задача 3
+        int year = 2400;
+        int year1 = year % 4;
+        int year2 = year % 100;
+        int year3 = year % 400;
+        {System.out.println(year2);}
+        if (year1 == 0 && year2 !=0)  {System.out.println("Год является високосный");
+        } else if (year3 == 0) {System.out.println("Год является високосный");}
 
 
     }
