84e3a827a1fd04234fa9a5e902db8901dd5f0329
Dmitry <d.spitsyn@internet.ru>
2022-05-20 08:40:09 +0300

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("homework");
+        int clientOS = 1;
+        if (clientOS == 1) {
+            System.out.println("Установить версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 0) {
+            System.out.println("Установить версию приложения для Android по ссылке");
+        }
+
     }
 }
