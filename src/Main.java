e91c96ca4b224fb543aa182199b491ca241a2214
Dmitry <d.spitsyn@internet.ru>
2022-05-20 12:46:21 +0300

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,12 @@ public class Main {
     public static void main(String[] args) {
         int clientOS = 1;
         int clientDeviceYear = 2014;
-        if (clientOS == 1 && clientDeviceYear > 2014) {
-            System.out.println("Установить версию приложения для Android по ссылке");
-        }
-        if (clientOS == 0 && clientDeviceYear > 2014) {
-            System.out.println("Установить  версию приложения для iOS по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear < 2015) {
-            System.out.println("Установить  облегченную приложения для Android по ссылке");
-        }
-        if (clientOS == 0 && clientDeviceYear < 2015) {
-            System.out.println("Установить облегченную версию приложения для iOS по ссылке");
-        }
+        if (clientOS == 1 && clientDeviceYear > 2014) {System.out.println("Установить версию приложения для Android по ссылке");}
+        if (clientOS == 0 && clientDeviceYear > 2014) {System.out.println("Установить  версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {System.out.println("Установить  облегченную приложения для Android по ссылке");}
+        if (clientOS == 0 && clientDeviceYear < 2015) {System.out.println("Установить облегченную версию приложения для iOS по ссылке");}
+
+
+
     }
 }
