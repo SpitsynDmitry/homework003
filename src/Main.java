b9eaa44025cb02ddf5d1405439b57655a4815e8c
Dmitry <d.spitsyn@internet.ru>
2022-05-20 12:33:05 +0300

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int clientOS = 1;
-        if (clientOS == 1) {
-            System.out.println("Установить версию приложения для iOS по ссылке");
-        }
-        if (clientOS == 0) {
+        int clientDeviceYear = 2014;
+        if (clientOS == 1 && clientDeviceYear > 2014) {
             System.out.println("Установить версию приложения для Android по ссылке");
         }
-
+        if (clientOS == 0 && clientDeviceYear > 2014) {
+            System.out.println("Установить  версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установить  облегченную приложения для Android по ссылке");
+        }
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установить облегченную версию приложения для iOS по ссылке");
+        }
     }
 }
